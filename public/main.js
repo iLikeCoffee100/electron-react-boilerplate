fa7b66e338404656c2c35de9f0c489764462e128
Kevin Khachi <coderkevin500@gmail.com>
2022-08-03 20:55:07 -0700

    "electron react boilerplate used for creating new desktop applications"

--- public/main.js
+++ public/main.js
@@ -62,12 +62,10 @@ ipcMain.on("anything-asynchronous", (event, arg) => {
 // Speed up the mouse.
 const fuck = () => {
   robot.setMouseDelay(2);
-
   var twoPI = Math.PI * 2.0;
   var screenSize = robot.getScreenSize();
   var height = screenSize.height / 2 - 10;
   var width = screenSize.width;
-
   for (var x = 0; x < width; x++) {
     let y = height * Math.sin((twoPI * x) / width) + height;
     robot.moveMouse(x, y);
